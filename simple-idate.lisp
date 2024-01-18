6d6c8f287ceaf937703a0c97bbcf44b95cf674bd
Marcus Pemer <marcus@pemer.com>
2024-01-17 21:14:19 -0500

    Added file header with license.

--- simple-idate.lisp
+++ simple-idate.lisp
@@ -1,3 +1,28 @@
+;;;; -----------------------------------------------------------------------
+;;;; Filename: simple-idate.lisp
+;;;; Author: Marcus Pemer
+;;;; Email: marcus@pemer.com
+;;;;
+;;;; Description:
+;;;; This file contains a collection of utility functions for use in Lisp
+;;;; applications. It includes functions for parsing numbers, manipulating
+;;;; hash tables, rounding numbers, processing lists with function chains
+;;;; and monadic transformations, and other general utility functions.
+;;;;
+;;;; Copyright (C) 2023 Marcus Pemer
+;;;; This program is free software: you can redistribute it and/or modify
+;;;; it under the terms of the GNU General Public License as published by
+;;;; the Free Software Foundation, either version 3 of the License, or
+;;;; (at your option) any later version.
+;;;;
+;;;; This program is distributed in the hope that it will be useful,
+;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;;; GNU General Public License for more details.
+;;;;
+;;;; You should have received a copy of the GNU General Public License
+;;;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+;;;; -----------------------------------------------------------------------
 (in-package #:simple-idate)
 
 
